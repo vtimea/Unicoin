fa9f0917b163661461bd106b6282efa9ab6e085a
VAJNATIMI\vajnatimi <vajnatimi@gmail.com>
2017-12-02 16:13:15 +0100

    notification builder

--- Unicoin/app/src/main/java/vajnatimi/unicoin/NotificationService.java
+++ Unicoin/app/src/main/java/vajnatimi/unicoin/NotificationService.java
@@ -1,9 +1,11 @@
 package vajnatimi.unicoin;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -20,7 +22,32 @@ public class NotificationService extends IntentService{
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         String s = intent.getExtras().getString("type");
-        Log.i("notif", "------->" + s);
-        Log.i("notif", "=========> NOTIFICATION!!!");
+        NotificationCompat.Builder mBuilder;
+        int mNotificationId = 001;
+        NotificationManager mNotifyMgr;
+        switch (s){
+            case "daily":
+                Log.i("notif", "========> DAILY NOTIFICATION");
+                mBuilder =
+                        new NotificationCompat.Builder(this)
+                                .setContentTitle("Unicoin reminder")
+                                .setSmallIcon(R.drawable.rc_notif)
+                                .setContentText("Don't forget to add today's transactions!");
+                mNotificationId = 001;
+                mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                break;
+            case "weekly":
+                Log.i("notif", "========> WEEKLY NOTIFICATION");
+                mBuilder =
+                        new NotificationCompat.Builder(this)
+                                .setContentTitle("Unicoin reminder")
+                                .setSmallIcon(R.drawable.rc_notif)
+                                .setContentText("Don't forget to add today's transactions!");
+                mNotificationId = 002;
+                mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                break;
+        }
     }
 }
